f20b7e3c9f7b76d425aabf2a5e9bc22486ef277a
Anatol <yarmolovich1986@gmail.com>
2019-05-20 11:24:10 +0300

    Initial Commit

---
+++ modalSeague/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  modalSeague
+//
+//  Created by Anatol Uarmolovich on 5/20/19.
+//  Copyright © 2019 Anatol Uarmolovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
