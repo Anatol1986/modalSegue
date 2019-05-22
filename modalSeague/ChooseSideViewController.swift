5a4f3c4a91e6bea621655921dc676a917f9daf81
Anatol <yarmolovich1986@gmail.com>
2019-05-22 09:42:12 +0300

    last version

---
+++ modalSeague/ChooseSideViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ChooseSideViewController.swift
+//  modalSeague
+//
+//  Created by Anatol Uarmolovich on 5/20/19.
+//  Copyright © 2019 Anatol Uarmolovich. All rights reserved.
+//
+
+import Foundation
